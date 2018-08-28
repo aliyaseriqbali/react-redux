abf9106cf03fac6bd242e8dc834fffc73ffb3057
ali iqbali <ali.iqbali@frontedgeit.se>
2018-08-28 14:37:25 +0200

    Added video list with video list items

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search-bar'
-
+import VideoList from './components/video-list'
+import VideoDetail from './components/video-detail'
 
 const API_KEY = 'AIzaSyDUkHY0TGbqT0Js_2Anmjk-q0_lEplmfvg';
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
+    })
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos} />
+      </div>
+    )
+  }
 }
 
 // Take this HTML component's generated HTML and put it on the page (in the DOM)
