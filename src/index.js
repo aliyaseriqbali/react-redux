608c6b174e89ba8bf56e97252c8e40a34f5271ed
ali iqbali <ali.iqbali@frontedgeit.se>
2018-08-29 08:42:21 +0200

    Fixed the search bar so that it can accept any terms

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import _ from 'lodash'
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search-bar'
 import VideoList from './components/video-list'
 import VideoDetail from './components/video-detail'
-
 const API_KEY = 'AIzaSyDUkHY0TGbqT0Js_2Anmjk-q0_lEplmfvg';
 
 // Create a new component. This component should produce some HTML
@@ -16,18 +16,25 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     }
+    this.videoSearch('surfboards')
+  }
 
-    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
       })
     })
   }
+
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
+
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
